190e462be97f37ddcb255597a1edd612c3552a6f
Pedro Cardenas <pedrocardenasrestrepo@gmail.com>
2024-03-27 18:19:39 -0500

    Encode base62

---
+++ app/models/short_code.rb
@@ -0,0 +1,20 @@
+class ShortCode
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz".freeze
+  BASE = ALPHABET.length
+
+  def self.encode(number)
+    return ALPHABET.first if number.zero? || number.nil?
+    result = ""
+    while number > 0 do
+      index = number % BASE
+      char = ALPHABET[index]
+      result.prepend char
+      number = number / BASE
+    end
+
+    result
+  end
+
+  def self.decode(number)
+  end
+end
