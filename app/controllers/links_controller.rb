947929cb3a28a0d72357f2d31de47d27b727e906
Pedro Cardenas <pedrocardenasrestrepo@gmail.com>
2024-03-30 12:19:52 -0500

    Url counter and chart

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,15 @@
 class LinksController < ApplicationController
+before_action :set_link, only: [:show]
+
   def index
     @links = Link.recent_first
   end
+
+  def show
+
+  end
+
+
   def create
     @link = Link.new(link_params)
     if @link.save
@@ -17,6 +25,10 @@ class LinksController < ApplicationController
 
   def link_params
     params.require(:link).permit(:url)
+  end
+
+  def set_link
+    @link = Link.find(params[:id])
   end
 
 end
---
+++ app/models/view.rb
@@ -0,0 +1,4 @@
+class View < ApplicationRecord
+  belongs_to :link, counter_cache: true
+
+end
